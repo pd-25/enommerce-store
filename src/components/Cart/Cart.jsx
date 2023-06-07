a5a83d4ab6fb250ec5925f609aa5e10ce8d0757b
Pradipta Bhuin <pradipta.bhuin@unifiedinfotch.net>
2023-06-07 13:53:51 +0530

    working on cartItem

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import "./Cart.scss";
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
+import CartItem from "./CartItem/CartItem";
 const Cart = ({setShowCart}) => {
     return <div className="cart-panel">
         <div className="opac-layer"></div>
@@ -13,11 +14,27 @@ const Cart = ({setShowCart}) => {
                 </span>
             </div>
 
-            <div className="empty-cart">
+            {/* <div className="empty-cart">
                 <BsCartX />
                 <span>No products in the cart</span>
                 <button className="return-cta">RETURN TO SHOP</button>
+            </div> */}
+
+            <>
+            <CartItem />
+
+            <div className="cart-footer">
+                <div className="subtotal">
+                    <span className="text">Subtotal:</span>
+                    <span className="text total">&#8377;348</span>
+                </div>
+                <div className="button">
+                    <button className="checkout-cta">
+                        Checkout
+                    </button>
+                </div>
             </div>
+            </>
         </div>
     </div>;
 };
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,14 @@
 import "./CartItem.scss";
+import { MdClose } from "react-icons/md";
+import prodImg from "../../../assets/products/watch-prod-3.webp";
 const CartItem = () => {
-    return <div>Cart item</div>;
+    return <div className="cart-products">
+        <div className="cart-product">
+            <div className="img-container">
+                <img src={prodImg} alt="" />
+            </div>
+        </div>
+    </div>;
 };
 
 export default CartItem;
